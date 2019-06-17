07de2b9eb0714b7c1fee009c8a81cf11b1518004
Israel-ICM <israel.jcm@gmail.com>
2019-06-16 21:20:26 -0400

    Inicio

---
+++ src/curso/Curso.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package curso;
+
+/**
+ *
+ * @author IsraelCM
+ */
+public class Curso {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ventana v=new ventana();
+        v.setVisible(true);
+    }
+}
